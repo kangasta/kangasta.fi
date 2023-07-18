6f8e46cbed7e35dba53c60594c8b3e8531f6c2fb
Toni Kangas <toni.ar.kangas@gmail.com>
2023-07-19 00:50:54 +0300

    Remove `-it` from `docker run` command

--- prepare.sh
+++ prepare.sh
@@ -5,6 +5,6 @@ mkdir -p public/;
 cp favicon.png index.html profile-512px.jpg public/;
 
 docker build ./scripts/preview/ -t preview;
-docker run -it --name preview -v $(pwd)/public:/public preview;
+docker run --name preview -v $(pwd)/public:/public preview;
 docker cp preview:/home/pptruser/preview.png ./public/
 docker rm preview;
